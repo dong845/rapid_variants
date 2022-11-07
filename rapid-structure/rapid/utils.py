ee7322ffa562b1160255c27369ae00c82ddda66b
Leo-Lyu <55332769+dong845@users.noreply.github.com>
2022-11-07 13:54:45 +0100

    Update utils.py

--- rapid-structure/rapid/utils.py
+++ rapid-structure/rapid/utils.py
@@ -152,3 +152,56 @@ class MlpPolicy_cnn2d(object):
         self.vf = vf
         self.step = step
         self.value = value
+      
+class MlpPolicy_impala(object):
+    def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, reuse=False):
+        self.pdtype = make_pdtype(ac_space)
+        with tf.variable_scope("model", reuse=reuse):
+            X = tf.placeholder(tf.float32, shape=(None,)+ob_space.shape)
+            activ = tf.tanh
+
+            def feat_extract(x):
+                active1 = tf.nn.elu
+                processed_x = active1(tf.layers.conv2d(
+                    x, 32, 3, 1, padding="same"))
+                processed_x = active1(tf.layers.conv2d(processed_x, 32, 3, 1))
+                processed_x = active1(tf.layers.conv2d(processed_x, 32, 3, 1))
+                return processed_x
+
+            def fcs(x):
+                pi_h1 = activ(fc(x, 'fc1', nh=128, init_scale=np.sqrt(2)))
+                pi_h1 = activ(
+                    fc(pi_h1, 'fc2', nh=128, init_scale=np.sqrt(2)))
+                return pi_h1
+
+            processed_x = feat_extract(X)
+            processed_x = tf.layers.flatten(processed_x)
+            co_x = fcs(processed_x)
+
+            pi_h1 = activ(fc(co_x, 'pi_fc1',
+                          nh=64, init_scale=np.sqrt(2)))
+            pi_h2 = activ(fc(pi_h1, 'pi_fc2', nh=64, init_scale=np.sqrt(2)))
+            vf_h1 = activ(fc(co_x, 'vf_fc1',
+                          nh=64, init_scale=np.sqrt(2)))
+            vf_h2 = activ(fc(vf_h1, 'vf_fc2', nh=64, init_scale=np.sqrt(2)))
+            vf = fc(vf_h2, 'vf', 1)[:, 0]
+
+            self.pd, self.pi = self.pdtype.pdfromlatent(pi_h2, init_scale=0.01)
+
+        a0 = self.pd.sample()
+        neglogp0 = self.pd.neglogp(a0)
+        self.initial_state = None
+
+        def step(ob, *_args, **_kwargs):
+            tf.set_random_seed(0)
+            a, v, neglogp, = sess.run([a0, vf, neglogp0], {X: ob})
+            return a, v, self.initial_state, neglogp
+
+        def value(ob, *_args, **_kwargs):
+            return sess.run(vf, {X: ob})
+
+        self.X = X
+        self.vf = vf
+        self.step = step
+        self.value = value
+        
